75cceda2de77993576586f859dba99608942724b
Peiving1 <Eivind@gullbra.net>
2025-11-09 18:26:45 +0100

    all the required files

---
+++ Inc/main.h
@@ -0,0 +1,87 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define POT_IN2_Pin GPIO_PIN_0
+#define POT_IN2_GPIO_Port GPIOC
+#define POT_IN1_Pin GPIO_PIN_3
+#define POT_IN1_GPIO_Port GPIOA
+#define ENA_Pin GPIO_PIN_9
+#define ENA_GPIO_Port GPIOE
+#define ENA2_Pin GPIO_PIN_11
+#define ENA2_GPIO_Port GPIOE
+#define IN_1A_Pin GPIO_PIN_3
+#define IN_1A_GPIO_Port GPIOB
+#define IN_2A_Pin GPIO_PIN_5
+#define IN_2A_GPIO_Port GPIOB
+#define IN_4A_Pin GPIO_PIN_8
+#define IN_4A_GPIO_Port GPIOB
+#define IN_3A_Pin GPIO_PIN_9
+#define IN_3A_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
